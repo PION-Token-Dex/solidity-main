a4dd8617832d54b5839bfef4732358ebb2d968d9
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-13 06:53:19 -0400

    Update Exchange.java

--- Exchange.java
+++ Exchange.java
@@ -1,6 +1,7 @@
 package Exchange;
 
 import ActiveIndexes.ActiveIndexes;
+import Require.Require;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,7 @@ public class Exchange {
 
     String pionAddress = "0xPION";
     public Map<String, ActiveIndexes> tokenIndexes = new HashMap();
+    public final Require rq = new Require(); //for internal use as solidity's require!
 
     public void setPionAddress(String pionAddress){ //onlyowner
         this.pionAddress = pionAddress;
@@ -51,6 +53,21 @@ public class Exchange {
     public int getWithdrawSellData(String forToken, String userAddress, int priceIndex) throws Exception {
         return tokenIndexes.get(forToken).getWithdrawAmountSell(userAddress, priceIndex);
     }
+    //--------------------------------
+    public int token2TokenCalculate(String sellToken, String buyToken, int amount) throws Exception {
+        int pions = tokenIndexes.get(sellToken).currentToPionConversion(amount);
+        int buyTokens = tokenIndexes.get(buyToken).currentToTokenConversion(pions);
+        return buyTokens;
+    }
+    //how many current peons are circulating
+    public int token2TokenGetPionAmount(String sellToken) throws Exception {
+        return tokenIndexes.get(sellToken).getCurrentSellAmount();
+    }
+    //how many tokens user is asking for are circulating
+    public int token2TokenGetTokenAmount(String buyToken) throws Exception {
+        return tokenIndexes.get(buyToken).getCurrentBuyAmount();
+    }
+
 
 
 }
