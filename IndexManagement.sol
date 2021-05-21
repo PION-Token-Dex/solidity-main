54164609d992cd7eff7e93a244ece0fcdb96afbd
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-20 22:49:35 -0400

    Create IndexManagement.sol

---
+++ IndexManagement.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+
+
+contract IndexManagement{
+struct Indexes{
+    mapping(uint => address)  tokenMap; //id, tokenAddress
+    mapping(uint=>uint)  priceIndex; //id, priceIndex
+    uint lastId;
+    uint lastActiveId;
+}
+
+mapping(address=>Indexes) private userIndexes;  //user address, indexes
+
+function addIndex(address userAddress, address tokenAddress, uint priceIndex) private {
+    uint lastId = userIndexes[userAddress].lastId;
+    ++lastId;
+    userIndexes[userAddress].lastId = lastId;
+    userIndexes[userAddress].tokenMap[lastId] = tokenAddress;
+    userIndexes[userAddress].priceIndex[lastId] = priceIndex;
+}
+
+function moveLastActiveIndex(address userAddress, uint toIndex) private {
+    userIndexes[userAddress].lastActiveId = toIndex;
+}
+
+function getTokenPriceIndexes(address userAddress, uint maxIndexes) private view returns(uint[] memory rt){
+    uint[] memory ret = new uint[](maxIndexes);
+    uint from = userIndexes[userAddress].lastActiveId;
+    uint to = userIndexes[userAddress].lastId;
+    uint t = 0;
+    for(;from<=to;from++){
+        if(userIndexes[userAddress].tokenMap[from]==userAddress){
+            ret[t] = userIndexes[userAddress].priceIndex[from];
+            ++t;
+            if(t==maxIndexes) break;
+        }
+    }
+    return ret;
+}
+
+    
+}
