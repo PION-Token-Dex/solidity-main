e8e0242e87e3baf7ba6f39b05881baa90f822d97
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-22 11:55:25 -0400

    Update Registration.sol

--- Registration.sol
+++ Registration.sol
@@ -7,12 +7,10 @@ abstract contract NewToken {
   function owner() public view virtual returns(address);
 }
 
-
 contract Registration is Ownable {
-    
-//User can register a token using the owner account, if and only if they can make a call to "owner()" function.
-//Otherwise, they have to contact the exchange owner for a listing.
 
+  //User can register a token using the owner account, if and only if they can make a call to "owner()" function.
+  //Otherwise, they have to contact the exchange owner for a listing.
 
   mapping(address => string) private tokenName;
   mapping(address => string) private tokenSymbol;
@@ -30,97 +28,99 @@ contract Registration is Ownable {
   mapping(uint => address) private IdToAddress;
   uint private tokenId;
   address private pionAddress;
-  
+
   constructor(address pionAddress_) {
     pionAddress = pionAddress_;
   }
 
   function addTokenAddress(address tokenAddress) external returns(bool) {
-      require(addressToId[tokenAddress]==0);
-      ++tokenId;
-      addressToId[tokenAddress] = tokenId;
-      IdToAddress[tokenId] = tokenAddress;
-      return true;
+    require(addressToId[tokenAddress] == 0);
+    ++tokenId;
+    addressToId[tokenAddress] = tokenId;
+    IdToAddress[tokenId] = tokenAddress;
+    return true;
   }
-  
+
   function getAddressId(address tokenAddress) external view returns(uint) {
     return addressToId[tokenAddress];
   }
-  
+
   function getIdAddress(uint id) external view returns(address) {
     return IdToAddress[id];
   }
-  
+
   function getId() external view returns(uint) {
     return tokenId;
-  }  
-  
+  }
+
   function setFlag1(address tokenAddress, uint flag) external onlyOwner returns(bool) {
     flag1[tokenAddress] = flag;
     return true;
   }
-  
+
   function changeTokenName(address tokenAddress, string memory tokenName_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     tokenName[tokenAddress] = tokenName_;
     return true;
   }
 
   function changeTokenSymbol(address tokenAddress, string memory tokenSymbol_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     tokenSymbol[tokenAddress] = tokenSymbol_;
     return true;
   }
 
   function changeTokenLogo(address tokenAddress, string memory logoURL_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     logoURL[tokenAddress] = logoURL_;
     return true;
   }
 
   function changeTokenWeb(address tokenAddress, string memory webURL_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     webURL[tokenAddress] = webURL_;
     return true;
   }
 
   function changeTokenSocial(address tokenAddress, string memory socialURL_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     socialURL[tokenAddress] = socialURL_;
     return true;
   }
 
   function changeTokenWhitepaper(address tokenAddress, string memory whitePaperURL_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     whitePaperURL[tokenAddress] = whitePaperURL_;
     return true;
   }
 
   function changeTokenDescription(address tokenAddress, string memory description_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     description[tokenAddress] = description_;
     return true;
   }
-  
+
   function changeExtra1(address tokenAddress, string memory extra) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+    requirePionOrFlag();
     extra1[tokenAddress] = extra;
     return true;
   }
-    function changeExtra2(address tokenAddress, string memory extra) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+
+  function changeExtra2(address tokenAddress, string memory extra) external returns(bool) {
+    requirePionOrFlag();
     extra2[tokenAddress] = extra;
     return true;
   }
-    function changeExtra3(address tokenAddress, string memory extra) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+
+  function changeExtra3(address tokenAddress, string memory extra) external returns(bool) {
+    requirePionOrFlag();
     extra3[tokenAddress] = extra;
     return true;
   }
-  
-  function registerToken(address tokenAddress, string memory tokenName_, string memory tokenSymbol_, string memory logoURL_, string memory webURL_, 
-                        string memory socialURL_, string memory whitePaperURL_, string memory description_) external returns(bool) {
-    require(flag1[msg.sender]!=10000 && msg.sender==pionAddress);
+
+  function registerToken(address tokenAddress, string memory tokenName_, string memory tokenSymbol_, string memory logoURL_, string memory webURL_,
+    string memory socialURL_, string memory whitePaperURL_, string memory description_) external returns(bool) {
+    requirePionOrFlag();
     tokenName[tokenAddress] = tokenName_;
     tokenSymbol[tokenAddress] = tokenSymbol_;
     logoURL[tokenAddress] = logoURL_;
@@ -130,10 +130,14 @@ contract Registration is Ownable {
     description[tokenAddress] = description_;
     return true;
   }
-  
- function getContractOwner(address tokenAddress) public view returns(address rt) {
+
+  function getContractOwner(address tokenAddress) public view returns(address rt) {
     NewToken newToken = NewToken(tokenAddress);
     return newToken.owner();
+  }
+
+  function requirePionOrFlag() private view {
+    require(flag1[msg.sender] != 10000 && msg.sender == pionAddress);
   }
 
 }
