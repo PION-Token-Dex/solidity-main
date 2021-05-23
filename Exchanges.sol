2365617d426f4594d82042f3060867ae822937fa
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-22 21:33:57 -0400

    Update Exchanges.sol

--- Exchanges.sol
+++ Exchanges.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-import "contracts/main/Ownable.sol";
-import "contracts/main/Exchange.sol";
+import "Ownable.sol";
+import "Exchange.sol";
 
 pragma solidity ^ 0.7 .0;
 
